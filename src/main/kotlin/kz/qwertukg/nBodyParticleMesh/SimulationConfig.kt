cc105a98a918dfc3cf383f0f5ef194af488e94d0
daniil <daniil.rakhmatulin@gmail.com>
2024-12-23 04:01:10 +0500

    galaxy collapse 2

--- src/main/kotlin/kz/qwertukg/nBodyParticleMesh/SimulationConfig.kt
+++ src/main/kotlin/kz/qwertukg/nBodyParticleMesh/SimulationConfig.kt
@@ -14,18 +14,18 @@ data class SimulationConfig(
     val worldWidth: Float = worldSize,
     val worldHeight: Float = worldSize,
     val worldDepth: Float = worldSize,
-    val potentialSmoothingIterations: Int = 120,
+    val potentialSmoothingIterations: Int = 120, // 60 is ok // 80 is ok // 100 is OK! // 120 is OK!!! // 300 is too long wait
     var g: Float = 100000f,
     var centerX: Float = (worldWidth * 0.5).toFloat(),
     var centerY: Float = (worldHeight * 0.5).toFloat(),
     var centerZ: Float = (worldDepth * 0.5).toFloat(),
-    val minRadius: Double = worldSize * 0.4 / 2,
-    val maxRadius: Double = worldSize * 0.5 / 2,
+    val minRadius: Double = worldSize * 0.2, // 0.2
+    val maxRadius: Double = worldSize * 0.25, // 0.2
     val massFrom: Float = 1.0f,
     val massUntil: Float = 1.0001f,
     val isDropOutOfBounds: Boolean = false,
     val fov: Float = 1f,
-    val magicConst: Float = 3.toFloat(),
+    val magicConst: Float = 3f, // 3f is ok
     val isFullScreen: Boolean = false,
     var blackHoleIndex: Int? = null
 )
